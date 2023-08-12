3c99849a4a5f5ecd2185bf2fe4180367ebe1ec07
R N Biswajit mallick <rnbiswajitmallick48@gmail.com>
2023-08-12 19:38:08 +0530

    added testng and able to run the code

--- src/Sumvalidation.java
+++ src/Sumvalidation.java
@@ -1,21 +1,27 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import files.payload;
 import io.restassured.path.json.JsonPath;
-
 public class Sumvalidation
 {
 	@Test
-	public void sumValidation() 
+	public void sumofcourses() 
 	{
+		int sum=0;
 		JsonPath js =new JsonPath(payload.coursePrice());
-		int count = js.getInt("cources.size()");
-		for(int i=0;i<=count;i++)
+		int count = js.getInt("courses.size()");
+		for(int i=0;i<count;i++)
 		{
 			int price=js.getInt("courses["+i+"].price");
 			int copies=js.getInt("courses["+i+"].copies");
-			int amount=js.getInt("courses["+i+"].amount");
+			int amount= price * copies;
+			System.out.println(amount);
+			sum=sum + amount;
 		}
+		System.out.println(sum);
+		int purchaseamount=js.getInt("dashboard.purchaseamount");
+		Assert.assertEquals(sum, purchaseamount);
 	}
 	
 
